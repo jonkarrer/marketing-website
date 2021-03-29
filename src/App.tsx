87a367b36a8a576b64f8841671f8af95cc5811d6
jonkarrer <karrerjon@gmail.com>
2021-03-29 14:32:05 -0500

    made landing modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Title from './comps/Title/Title';
 import SubTitle from './comps/SubTitle/SubTitle';
 import Feature from './comps/Feature/Feature';
 import Button from './comps/Button/Button';
+import Modal from './comps/Landing/Modal';
 import './App.css';
 
 function App() {
@@ -10,18 +11,23 @@ function App() {
     <div className="App">
       <Title />
       <SubTitle />
-      <div className="feature-wrapper">
-        <Feature image="./assets/left.jpg">100% Wool threads</Feature>
-        <Feature image="./assets/middle.jpg">Moisture wicking</Feature>
-        <Feature image="./assets/cute-sock.jpg">Artist designed</Feature>
+      <div className="content-wrapper">
+        <div className="feature-wrapper">
+          <Feature image="./assets/left.jpg">100% Wool threads</Feature>
+          <Feature image="./assets/middle.jpg">Moisture wicking</Feature>
+          <Feature image="./assets/cute-sock.jpg">Artist designed</Feature>
+        </div>
+        <div className="contact-wrapper">
+          <h3>
+            We are so glad you dropped by! Here is a special offer just for you.
+          </h3>
+          <Button pad={10} width={200}>
+            View Offer
+          </Button>
+        </div>
       </div>
-      <div className="contact-wrapper">
-        <h3>
-          We are so glad you dropped by! Here is a special offer just for you.
-        </h3>
-        <Button pad={10} width={200}>
-          View Offer
-        </Button>
+      <div className="modal-wrapper">
+        <Modal />
       </div>
     </div>
   );
---
+++ src/comps/Landing/Modal.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Modal.css';
+import Button from '../Button/Button';
+const Modal = () => {
+  return (
+    <div className="Modal">
+      <section className="left-side">
+        <article style={{ textAlign: 'left', margin: '20px' }}>
+          <h4>FREE DISCOUNT</h4>
+          <h1>Thank you for visiting our website</h1>
+          <p>
+            We love our socks and we hope that you will enjoy them as well. They
+            are perfect for hiking, jogging, or around the house. Use this
+            discount for your first purchase.
+          </p>
+        </article>
+        <div className="discount">
+          <h1>30% off</h1>
+          <p>Your first purchase</p>
+        </div>
+        <h3 style={{ marginBottom: '20px', marginLeft: '20px' }}>
+          Put your email below to recieve the coupon
+        </h3>
+        <form action="get" style={{ marginLeft: '20px' }}>
+          <input
+            className="email"
+            type="email"
+            placeholder="example@host.com"
+            required
+          />
+          <div className="button-wrapper">
+            <Button pad={10} width={150}>
+              Submit
+            </Button>
+          </div>
+        </form>
+      </section>
+      <section className="right-side">
+        <div className="close-modal">X</div>
+        <img src="./assets/moisture.jpg" alt="socks and heels" />
+      </section>
+    </div>
+  );
+};
+
+export default Modal;
