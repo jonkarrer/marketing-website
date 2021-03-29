ffda1ee362e702ac1001fde347f349937ff7b69f
jonkarrer <karrerjon@gmail.com>
2021-03-29 10:22:31 -0500

    updated styles v2

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ function App() {
       <Title />
       <SubTitle />
       <div className="feature-wrapper">
-        <Feature image="./assets/left.jpg">Made with 80% wool</Feature>
+        <Feature image="./assets/left.jpg">100% Wool threads</Feature>
         <Feature image="./assets/middle.jpg">Moisture wicking</Feature>
-        <Feature image="./assets/cute-sock.jpg">Artist designed prints</Feature>
+        <Feature image="./assets/cute-sock.jpg">Artist designed</Feature>
       </div>
       <div className="contact-wrapper">
         <h3>
